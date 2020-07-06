6b196dfcd87d4e8afbbd6a1b9810b7737417d921
Abis <abisali@hotmail.com>
2020-07-07 03:43:47 +0400

    Update main.py

--- main.py
+++ main.py
@@ -35,11 +35,16 @@ def scan_url():
     print(scan_id)
 
 def get_url():
+    step2()
     url = 'https://www.virustotal.com/vtapi/v2/url/report'
     params = {'apikey': '3eff5d05b40c79dc3cd3fb5c05e9bc1bc0bdc0b0dfc0b9c9846ca7daed1f4bd6', 'resource':'<resource>'}
     params['resource'] = scan_id
     response = requests.get(url, params=params)
-    print(response.json())
+    print("Total number of Engines:")
+    print(response.json().get('total'))
+    print("Total number of Issues:")
+    print(response.json().get('positives'))
+
 
 def open_file():
     global file_choose
@@ -62,7 +67,9 @@ def get_file():
     params = {'apikey': '3eff5d05b40c79dc3cd3fb5c05e9bc1bc0bdc0b0dfc0b9c9846ca7daed1f4bd6', 'resource': 'md5_hash'}
     params['resource'] = md5_hash
     response = requests.get(url_for_report, params=params)
+    print("Total number of Engines:")
     print(response.json().get('total'))
+    print("Total number of Issues:")
     print(response.json().get('positives'))
 
 
@@ -97,28 +104,45 @@ def file_scan_window():
 def url_scan_window():
     global url_scan_window
     url_scan_window = Toplevel()
-    url_scan_window.geometry("700x400")
+    url_scan_window.geometry("400x400")
 
     #Init Widgets
-    title_3 = Label(url_scan_window, text="TITLE")
-    send_url_button = Button(url_scan_window, text="Send", padx=80, pady=80, fg="white", bg="black", command=scan_url)
-    get_url_result_button = Button(url_scan_window, text="Get Result", padx=80, pady=80, fg="white", bg="black", command=get_url)
+    title_3 = Label(url_scan_window, text="URL Scanner", font=("Helvetica", 16))
+    send_url_button = Button(url_scan_window, text="Send", padx=80, pady=20, fg="white", bg="black", command=scan_url)
+    get_url_result_button = Button(url_scan_window, text="Get", padx=80, pady=20, fg="white", bg="black", command=get_url)
     exit_button_3 = Button(url_scan_window, text="EXIT", padx=80, pady=20, fg="white", bg="black", command=exit)
+    global bar
+    bar = ttk.Progressbar(url_scan_window, orient = HORIZONTAL, length = 400, mode='determinate')
     global input_field
-    input_field = Entry(url_scan_window, width=50, borderwidth=5)
+    input_field = Entry(url_scan_window,text="Enter URL", width=50, borderwidth=5)
+    space = Label(url_scan_window, text="" )
+    space2 = Label(url_scan_window, text="" )
+    space3 = Label(url_scan_window, text="" )
+    space4 = Label(url_scan_window, text="" )
 
     #Display Widgets
-    title_3.grid(row=0, column=0)
-    input_field.grid(row=2,column=3)
-    send_url_button.grid(row=1,column=1)
-    get_url_result_button.grid(row=1,column=3)
-    exit_button_3.grid(row=7,column=1)
+    title_3.grid(row=0, column=2)
+    space.grid(row=1,column=2)
+    bar.grid(row=2,column=2)
+    space2.grid(row=3,column=2)
+    input_field.grid(row=4,column=2)
+    send_url_button.grid(row=5,column=2)
+    space4.grid(row=6,column=2)
+    get_url_result_button.grid(row=7,column=2)
+    space3.grid(row=8,column=2)
+    exit_button_3.grid(row=9,column=2)
 
 
 def step():
     for x in range(5):
-        bar['value'] += 20
+        bar['value'] += 10
         file_scan_window.update_idletasks()
+        time.sleep(1)
+
+def step2():
+    for x in range(5):
+        bar['value'] += 10
+        url_scan_window.update_idletasks()
         time.sleep(1)
 
 
